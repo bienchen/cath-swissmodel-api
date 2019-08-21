5375108e1939dd3de58e2edfb0a77410aa31ee53
Stefan Bienert <stefan.bienert@unibas.ch>
2019-08-21 11:53:43 +0200

    Container: fetch user to be created for Django from environment variable

--- cathapi/Docker/postgres/10-create-django-user.sql
+++ cathapi/Docker/postgres/10-create-django-user.sql
@@ -1,1 +1,2 @@
-CREATE USER cathapiuser PASSWORD 'md5ac4bbe016b808c3c0b816981f240dcae' CREATEROLE LOGIN;
+\set cathapiuser `echo "$DJANGO_DB_USR"`
+CREATE USER :cathapiuser PASSWORD 'md5ac4bbe016b808c3c0b816981f240dcae' CREATEROLE LOGIN;
